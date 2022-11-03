d837a90bfd1c906e1f950c9ea36e3369542e605e
David Chaban <davidchaban01@gmail.com>
2022-11-02 23:38:55 -0300

    Add files Data : Activity,city and users

---
+++ src/data/activity.js
@@ -0,0 +1,130 @@
+let  dataActivity =   [
+    {        id: it1,
+        citiId: city6, 
+        name:"Samurai Museum",
+        photo:"{https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/b4/48/29/samurai-museum.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/9c/b9/6a/caption.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/05/f3/c5/20170726-142040-largejpg.jpg?w=1200&h=-1&s=1}",
+        description:"«2F» Katana(Japanese Sword)・Samurai Gallery・Exhibitions Displayed in Chronological Order・Samurai Costume Photo Studio" ,
+        price: 2500 ,
+        duration: 1  ,
+        userId: user2 },{
+
+            id: it1,
+        citiId: city6, 
+        name:"tour in Tokyo",
+        photo:"{https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/07/a1/61/9c.jpg,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/6b/75/6a/one-of-many-torri-at.jpg?w=1200&h=-1&s=1}",
+        description:"Full-Day Private and Custom Walking Tour in Tokyo" ,
+        price: 500 ,
+        duration: 2  ,
+        userId: user2 
+
+        },
+
+        {
+            id: it2 ,
+            cityId: city8,
+            name:"Stonehenge, el Castillo de Windsor",
+            photo:"{https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0b/74/4d/ab.jpg,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/bd/d0/83/caption.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/bd/d0/7a/caption.jpg?w=1200&h=-1&s=1}",
+            description: "tour to the ancestral home of the British royal family for over 900 years and the largest continuously occupied castle in Europe.",
+            price:3851,
+            duration:3,
+            userId: user1
+        },
+        {
+            id: it2 ,
+            cityId: city8,
+            name:"Tour Warner Bross - Harry Potter",
+            photo:"{https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0b/92/bd/30.jpg,https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0b/5c/4d/e8.jpg,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/b3/1f/23/caption.jpg?w=900&h=-1&s=1}",
+            description: "Follow in the footsteps of Harry Potter and explore the wonders of the wizarding world",
+            price:4231,
+            duration:4,
+            userId: user1
+        },
+
+        {
+            id: it3 ,
+            cityId: city9,
+            name:"Temple of the Reclining Buddha",
+            photo:"{https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/fa/67/3c/temple-of-the-reclining.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/fc/12/4f/temple-of-the-reclining.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/a0/e4/2f/caption.jpg?w=1200&h=-1&s=1}",
+            description: " Wat Pho is one of the top six temples in Thailand and is famous for the huge statue of the Reclining Buddha. By huge, we mean so big it looks like the temple was built around it because there’s really no way it could have been squeezed through the door. Besides the gigantic Reclining Buddha, the temple is well known for the numerous Buddha images all around it. In addition, the temple was the first center in Thailand for public education, offering science, religion and literature courses. It is located very close to the Grand Palace and the Temple of the Emerald Buddha. Thus, Wat Pho does not get as many tourists as other temples which makes it less crowded, perfect for new tourists in Thailand.",
+            price:5231,
+            duration:4,
+            userId: user3
+        },
+        {
+            id: it3 ,
+            cityId: city9,
+            name:"Bangkok City Tour of the Three Temples (SHA Plus)",
+            photo:"{https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/fa/e2/05.jpg,https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0a/ab/0b/a2.jpg,https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/06/fa/e1/f9.jpg}",
+            description: "This is a great tour for first-time visitors to Bangkok as you’ll learn all about the culture and religion of Thailand.",
+            price:7231,
+            duration:12,
+            userId: user3
+        },
+
+        {
+            id: it4 ,
+            cityId: city3,
+            photo:"{https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/07/96/0b/2a.jpg,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/71/d7/29/private-berlin-complete.jpg?w=1100&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/71/d6/56/private-berlin-complete.jpg?w=1200&h=-1&s=1}",
+            name:"Full-Day Private Tour of Berlin History",
+            description: "Tour of Berlin History",
+            price:3331,
+            duration:12,
+            userId: user1
+        },
+        {
+            id: it4 ,
+            cityId: city3,
+            name:" Berlin Wall",
+            photo:"{https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/ba/3b/01/photo0jpg.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/2c/14/27/what-is-left.jpg?w=1200&h=-1&s=1,https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/33/00/7d/berlin-whats-left-of.jpg?w=600&h=-1&s=1}",
+            description: "The Berlin Wall was a guarded concrete barrier that divided Berlin from 1961 to 1989. It encircled West Berlin, separating it from East German territory.",
+            price:4231,
+            duration:24,
+            userId: user1
+        },
+        {
+            id: it5 ,
+            cityId: city2,
+            name:"COLOSSEUM",
+            photo:"{https://roma-bella.com/wp-content/uploads/2016/02/colosseum.jpg,https://pbs.twimg.com/media/CWVeTO8WwAEVN_R?format=jpg&name=900x900,https://www.unmundopequenio.com/wp-content/uploads/2013/05/coliseo-dia-perspectiva.jpg}",
+            description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum .",
+            price:21200,
+            duration:12,
+            userId: user3
+        },
+        {
+            id: it5 ,
+            cityId: city2,
+            name:"VATICAN TOUR",
+            photo:"{https://images.unsplash.com/photo-1596966280261-e277cf0bc8da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80,https://images.unsplash.com/photo-1569759276108-31b8e7e43e7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80,https://images.unsplash.com/photo-1569758884145-6a2e8a00e539?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80}",
+            description: "The Vatican Private Tour 5 hours is a great way to really get the most out of the Vatican. Your own private guide and 5 hours of touring time you will see much .",
+            price:231,
+            duration:5,
+            userId: user3
+        },
+        {
+            id: it6 ,
+            cityId: city1,
+            name:"The National September 11 Memorial & Museum",
+            photo:"{https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQBfSUWHMoofdGBznsIS9TC0tJrvOegEoiz0xX696HEZ1ssBxA81mz0gqwD6HLGzQS4JlUSVIBN9rBgZeEqEATQKg,https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcR7QxzcH8uruSuoejSRF4z2b9fTQ-SPnHI_46UVfF3NHO1-PR5X74iQwV5k3Q7QNQhjDHL3uIPaKZ5LJWEpdi91nw,https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcS_K9tCmyLqpC1V_cu0RZ-svevfwFs9hQ9E5piolvP5BlFwKrvShUhsbxooBW_zLDbdcUX4CVruvu9RRzM1E8DmYA}",
+            description: "A temporary memorial was put up right after the attacks on the World Trade Center to remember the victims and fallen heroes of the attack. The winner of the design for the World Trade Center Site Memorial competition was Israeli-American architect Michael Arad of Handel Architects, a San Francisco and New York firm.",
+            price:50,
+            duration:24,
+            userId: user2
+        },
+        {
+            id: it6 ,
+            cityId: city1,
+            name:"The Statue of Liberty",
+            photo:"{https://www.washingtonian.com/wp-content/uploads/2021/06/iStock-1093281524.jpg,https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2021/09/640/320/statue-of-liberty-1.jpg?ve=1&tl=1,https://www.geographicguide.com/united-states/nyc/images/liberty-island-ny.jpg}",
+            description: "Liberty Enlightening the World, known as the Statue of Liberty, is one of the most famous landmarks in New York, the United States, and around the world. It is located on Liberty Island south of Manhattan Island, next to the mouth of the Hudson River and near Ellis Island. The Statue of Liberty was a gift from the French people to the American people in 1886 to commemorate the centennial of the United States Declaration of Independence and as a sign of friendship between the two nations.",
+            price:5231,
+            duration:2,
+            userId: user2
+        }
+    ]
+
+
+       
+
+
+
---
+++ src/data/users.js
@@ -0,0 +1,52 @@
+let dataUsers = [
+    {
+        id: "user1",
+        name: "Juan",
+        lastName: "Cismo",
+        age: 23,
+        email: "juan_cismo@hotmail.com",
+        password: "caminodeva",
+        code: "919293",
+        verified: true,
+        logged: true,
+    },
+    {
+        id: "user2",
+        name: "Emilia",
+        lastName: "Okada",
+        age: 21,
+        email: "emiokada@gmail.com",
+        password: "hima92022",
+        code: "011001",
+        verified: true,
+        logged: true,
+    },
+    {
+        id: "user3",
+        name: "Santiago",
+        lastName: "Velazquez",
+        age: 35,
+        email: "santi.velaz@gmail.com",
+        password: "shinden",
+        code: "33ff33",
+        verified: true,
+        logged: true,
+    },
+    {
+        id: "user4",
+        name: "Roberto",
+        lastName: "lanfranchi",
+        age: 26,
+        email: "rober@gmail.com",
+        password: "muykamui",
+        code: "66jj66",
+        verified: true,
+        logged: true,
+    },
+];
+
+
+
+
+
+
