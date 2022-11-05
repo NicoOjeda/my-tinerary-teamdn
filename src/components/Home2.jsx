235bcd88462566bc06b77171c8b5fad7aeaeb308
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-04 22:46:36 -0300

    se agregan componentes de carousel

---
+++ src/components/Home2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Home2.css'
+import CarouselData from './CarouselData'
+
+export default function Home2() {
+  return (
+    <div className='Home2-container'>
+      <CarouselData/>
+    </div>
+  )
+}
