95394c2405756b25358761f8b8f383721e95f01c
David Chaban <davidchaban01@gmail.com>
2022-11-05 01:55:49 -0300

    Se agrega cambio en el callToAction

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,39 +1,23 @@
 import React from 'react'
-
 import '../styles/home1.css';
-import{Link as LinkRouter} from "react-router-dom"
-import Navbar from './Navbar';
+import CallToAction from './CallToAction';
 
 
 export default function Home1() {
-
   const tittle = "Mytinerary";
   const slogan = "To travel is to live"
-
-
-
-
 return(
 
-
 <>
-
-            {/* <Navbar/> */}
+        
 <div className="home-container">
-{/* <div className="title-slogan"> */}
+
             <h1 className="tittle-home"><span className='my-style'> {tittle}</span></h1>
             <p className='slogan-home'> {slogan}</p>
-          {/* </div> */}
-          <div className='btn-container'>
-          <LinkRouter to="/Navbar">
-          <button className='btn-1'>Cities</button>
-          </LinkRouter>
-          <LinkRouter to="/Cities">
-          <button className='btn-1'>Hotels</button> 
-          </LinkRouter>
        
-</div>
-</div>
+          <CallToAction/>
+
+ </div>
 
 </>
 
