4b4aa74392dc39f44de6c0c4306665b6edb2037c
David Chaban <davidchaban01@gmail.com>
2022-11-04 22:29:26 -0300

    Se agrega Navbar,AutoTotop y CallToAction

---
+++ src/components/Burguer.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "../styles/burguer.css";
+import "../styles/navbar.css";
+import { useState } from "react";
+import { Link as LinkRouter } from "react-router-dom";
+
+
+  
+export default function Burguer() {
+    const btn = "Home";
+    const btn2 = "Cities";
+    const btn3 = "Hotels";
+
+
+
+  let [viewHide, setViewHide] = useState(false);
+  let hide = () => {
+    setViewHide(!viewHide);//viewHide = mostrarOcultar
+
+    console.log(viewHide)
+    
+};
+    return (
+     <>
+
+      <img className="logo1" src="https://i.imgur.com/lGiu6NV.png" alt="" />
+      <div className="nav-container1">
+      <div className="nav-container">
+        <div className="nav-0">
+          <LinkRouter to="/Index">
+            <button className="nav-1">{btn}</button>
+          </LinkRouter>
+          <LinkRouter to="/Cities">
+            <button className="nav-2"> {btn2}</button>
+          </LinkRouter>
+          <LinkRouter to="/Hotels">
+            <button className="nav-2"> {btn3}</button>
+          </LinkRouter>
+        </div>
+        
+
+
+
+
+        {
+         viewHide ?
+          
+          (<>
+          <div className="burguer-container">
+            <img
+              className="burguer-img"
+              src="https://cdn-icons-png.flaticon.com/512/7134/7134106.png"
+              alt="burguer foto"
+              onClick={hide}
+            />
+          <LinkRouter to="/Index">
+          <button className="btn-burguer-1">
+            Home
+          </button>
+          </LinkRouter>
+          <LinkRouter to="/home2">
+          <button className="btn-burguer-2">
+            Cities
+          </button>
+          </LinkRouter>
+          <LinkRouter to="/home2">
+          <button className="btn-burguer-3">
+            Hotels
+          </button>
+          </LinkRouter>
+
+          </div>
+            </>
+
+        ) : (
+          <>
+            <img
+              className="burguer-img "
+              src="https://cdn-icons-png.flaticon.com/512/7134/7134106.png"
+              alt="burguerIcon"
+              onClick={hide}
+            />
+          </>
+        )}
+
+
+</div>
+</div>
+
+     </> 
+        );
+        }
