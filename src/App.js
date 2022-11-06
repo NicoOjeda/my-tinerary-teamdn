00e6cd03b08b53e575b63a719a9cf0e4a4ead6ce
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-06 16:13:50 -0300

    se agrega ruta de newhotel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Home from "./pages/Home";
 import {Routes, Route } from 'react-router-dom'
 import WebSiteLayout from './layouts/WebSiteLayout'
-import NotFound from "./components/NotFound";
+import NotFound from "./pages/NotFound";
 
 import SignUp from "./components/SignUp"
 
 
-import Signin from "./pages/Signin";
+import Signin from "./components/SignInForm";
+import NewHotel from "./components/NewHotelForm";
 
 
 
@@ -16,21 +17,15 @@ function App() {
   return (
     <WebSiteLayout>
       <Routes> 
-
       <Route path='/' element={<Home/>} ></Route>
       <Route path='*' element={<NotFound/>} ></Route>
       <Route path='/SignUp' element={<SignUp/>} ></Route>
-    
-      </Routes>
-  </WebSiteLayout>
-      
-
-        <Route path='/' element={<Home/>} ></Route>
-        <Route path='*' element={<NotFound/>} ></Route>
-      
+      <Route path='/' element={<Home/>} ></Route>
+      <Route path='*' element={<NotFound/>} ></Route>
+      <Route path='/newhotel' element={<NewHotel/>}></Route>
 
         
-        <Route path='/signin' element={<Signin/>} ></Route>
+      <Route path='/signin' element={<Signin/>} ></Route>
       </Routes>
     </WebSiteLayout>
 
