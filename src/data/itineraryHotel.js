ab26ac2fc0f32136f52d7819dc3a6c8fa6682a8f
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-07 07:17:39 -0300

    se modifica fecha para renderizado correcto

--- src/data/itineraryHotel.js
+++ src/data/itineraryHotel.js
@@ -6,7 +6,7 @@ let itineraryHotel = [
         description: "7a de Bruckner, Mozart | van Zweden, Bronfman, New York Philharmonic",
         photo: "https://res.cloudinary.com/nyphil/image/upload/c_scale,w_500/v1439322023/images/shared/david-geffen-hall/the-hall.jpg",
         price: "100",
-        date: 2022-11-12,
+        date: "2022-11-12",
         userId:  "user1" 
     },
     {
@@ -16,7 +16,7 @@ let itineraryHotel = [
         description: "The Coronas are an Irish rock band that originated in Dublin.",
         photo: "https://rockfm-cdnmed.agilecontent.com/resources/jpg/5/0/1584540554205.jpg",
         price: "75",
-        date: 2022-12-5,
+        date: "2022-12-5",
         userId:  "user2" 
     },
     {
@@ -26,7 +26,7 @@ let itineraryHotel = [
         description: "Rainbow Kitten Surprise is an alternative rock indie band, featuring lead vocalist Ela Melo, Darrick Keller (guitar, backup vocals)",
         photo: "https://www.centralpark.com/downloads/9806/download/Kitten-Surprise.jpg?cb=e3a3673d6345bef6cfc29814f724e35b&w=1200",
         price: "62",
-        date: 2022-11-9,
+        date: "2022-11-9",
         userId:  "user2" 
     },
     {
@@ -36,7 +36,7 @@ let itineraryHotel = [
         description: "Sampa the Great, is a Zambian singer, rapper and songwriter.",
         photo: "https://i.ytimg.com/vi/MddG0quhiFY/maxresdefault.jpg",
         price: "75",
-        date: 2023-1-5,
+        date: "2023-1-5",
         userId:  "user3" 
     },
     {
@@ -46,7 +46,7 @@ let itineraryHotel = [
         description: "Il Divo is a musical group comprised of a vocal quartet of male opera singers",
         photo: "https://lastfm.freetls.fastly.net/i/u/ar0/91728e9801eedfbb24371cd4cef3660b.jpg",
         price: "62",
-        date: 2022-11-10,
+        date: "2022-11-10",
         userId:  "user1" 
     },
     {
@@ -56,7 +56,7 @@ let itineraryHotel = [
         description: "Motomami World Tour is the third concert tour by Spanish singer Rosalía in support of her third studio album.",
         photo: "http://enagenda.com.ar/uploads/noticias/5/2022/04/20220418124019_rosalia.jpg",
         price: "63",
-        date: 2023-1-9,
+        date: "2023-1-9",
         userId:  "user4" 
     },
     {
@@ -66,7 +66,7 @@ let itineraryHotel = [
         description: "Electronic music producer and DJ his style goes beyond House, Techno and Disco",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBeS4bGN39-uRXOU0-aUJgGXs496tbfNo2voFVUcI3tkl554Bhxe-RA3wN-TmKx_M-c6I&usqp=CAU",
         price: "95",
-        date: 2023-2-10,
+        date: "2023-2-10",
         userId:  "user4" 
     },
     {
@@ -76,7 +76,7 @@ let itineraryHotel = [
         description: "Halestorm is an American rock band from Red Lion, Pennsylvania, consisting of lead vocalist and guitarist Lzzy Hale.",
         photo: "https://www.billboard.com/wp-content/uploads/2022/07/Halestorm-press-2022-cr-jimmy-fontaine-billboard-1548.jpg?w=942&h=623&crop=1",
         price: "63",
-        date: 2023-1-19,
+        date: "2023-1-19",
         userId:  "user2" 
     },
     {
@@ -86,7 +86,7 @@ let itineraryHotel = [
         description: "Sepultura is a Brazilian thrash metal band formed in 1984 in Belo Horizonte.",
         photo: "https://arc-anglerfish-arc2-prod-elespectador.s3.amazonaws.com/public/QYVXVBCK7NDQ7A2HQJZN2ZZSHE.jpg",
         price: "40",
-        date: 2023-2-1,
+        date: "2023-2-1",
         userId:  "user1" 
     },
     {
@@ -96,7 +96,7 @@ let itineraryHotel = [
         description: "AL PACINO UK TOUR 2023! · An Exclusive Live on-stage interview with the man himself, Al Pacino.",
         photo: "https://southpasadenan.com/wp-content/uploads/south-pasadena-news-06-14-2022-al-pacino.jpg",
         price: "72",
-        date: 2022-12-11,
+        date: "2022-12-11",
         userId:  "user3" 
     },
     {
@@ -106,7 +106,7 @@ let itineraryHotel = [
         description: "A majestic live adventure that, will take EROS RAMAZZOTTI to some of the most important cities in the world",
         photo: "https://www.eldiariodelapampa.com.ar/img/publicaciones/2022/07/07/alta/34809_0xh.jpg",
         price: "80",
-        date: 2022-12-2,
+        date: "2022-12-2",
         userId:  "user4" 
     },
     {
@@ -116,7 +116,9 @@ let itineraryHotel = [
         description: "Luísa's music is characterized by being pop, but the singer adds some elements to give identity to her sound",
         photo: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/HHTVGKXK3JHH5OVNGP7GNMV6AQ.jpg",
         price: "75",
-        date: 2022-12-10,
+        date: "2022-12-10",
         userId:  "user3" 
     }
 ];
+
+export default itineraryHotel
