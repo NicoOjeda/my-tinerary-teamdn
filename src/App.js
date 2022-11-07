c336e2d6330989100b92a3f9051b8d4df225ff76
David Chaban <davidchaban01@gmail.com>
2022-11-07 02:27:53 -0300

    Se resuelven errores y se agregan futuras rutas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Home from "./pages/Home";
 import {Routes, Route } from 'react-router-dom'
 import WebSiteLayout from './layouts/WebSiteLayout'
 import NotFound from "./components/NotFound";
-
-import SignUp from "./components/SignUp"
-
-
+import SignUp from "./pages/SignUp"
 import Signin from "./pages/Signin";
-
+// import NewHotel from "./components/NewHotelForm";
+// import Hotels from "./pages/Hotels";
+// import Details from "./pages/Details";
+import Cities from './pages/Cities';
 
 
 
@@ -16,21 +16,19 @@ function App() {
   return (
     <WebSiteLayout>
       <Routes> 
-
       <Route path='/' element={<Home/>} ></Route>
       <Route path='*' element={<NotFound/>} ></Route>
+      {/* <Route path='/newhotel' element={<NewHotel/>}></Route>
+      <Route path='/hotels' element={<Hotels/>}></Route> */}
       <Route path='/SignUp' element={<SignUp/>} ></Route>
-    
-      </Routes>
-  </WebSiteLayout>
-      
+      <Route path='/signin' element={<Signin/>} ></Route>
+      <Route path='/Cities' element={<Cities/>} ></Route>
+      {/* <Router path="/details/:id" element={<Details/>}></Router> */}
 
-        <Route path='/' element={<Home/>} ></Route>
-        <Route path='*' element={<NotFound/>} ></Route>
-      
 
-        
-        <Route path='/signin' element={<Signin/>} ></Route>
+
+
+
       </Routes>
     </WebSiteLayout>
 
---
+++ src/pages/Details.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import { useParams } from "react-router-dom";
+
+
+// export default function Details() {
+//     // let params = useParams()
+//     // console.log(params);
+//     let {id} = useParams()
+//     console.log(id);
+//   return (
+//     <div>{id}</div>
+//   )
+// }
