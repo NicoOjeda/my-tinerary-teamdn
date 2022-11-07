ed80429b0192c1159472adda59c3a5c27cc20593
David Chaban <davidchaban01@gmail.com>
2022-11-07 03:01:02 -0300

    -

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import WebSiteLayout from './layouts/WebSiteLayout'
 import NotFound from "./components/NotFound";
 import SignUp from "./pages/SignUp"
 import Signin from "./pages/Signin";
-// import NewHotel from "./components/NewHotelForm";
-// import Hotels from "./pages/Hotels";
-// import Details from "./pages/Details";
+
 import Cities from './pages/Cities';
 
 
@@ -18,12 +16,11 @@ function App() {
       <Routes> 
       <Route path='/' element={<Home/>} ></Route>
       <Route path='*' element={<NotFound/>} ></Route>
-      {/* <Route path='/newhotel' element={<NewHotel/>}></Route>
-      <Route path='/hotels' element={<Hotels/>}></Route> */}
+      
       <Route path='/SignUp' element={<SignUp/>} ></Route>
       <Route path='/signin' element={<Signin/>} ></Route>
       <Route path='/Cities' element={<Cities/>} ></Route>
-      {/* <Router path="/details/:id" element={<Details/>}></Router> */}
+     
 
 
 
