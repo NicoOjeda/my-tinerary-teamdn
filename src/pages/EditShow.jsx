981a7d0ed0301244e34ddca1745aaff37e4aba5a
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-22 09:10:31 -0300

    se crea pagina editshow

---
+++ src/pages/EditShow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EditShowForm from '../components/EditShowForm'
+
+export default function EditShow() {
+  return (
+    <div>
+      <EditShowForm/>
+    </div>
+  )
+}
