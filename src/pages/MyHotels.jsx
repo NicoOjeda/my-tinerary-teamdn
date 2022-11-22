d466448981c64d53906ac7c277c6fe8f90f1ecd0
Nico ojeda <nico_ojeda86@hotmail.com>
2022-11-22 04:01:08 -0300

    se crea pagina myhotels

---
+++ src/pages/MyHotels.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyHotelslist from '../components/MyHotelslist'
+
+export default function MyHotels() {
+  return (
+    <div>
+      <MyHotelslist/>
+    </div>
+  )
+}
