409f8318a0e1b8af2368235ad109cc97587567f1
David Chaban <davidchaban01@gmail.com>
2022-11-27 19:10:16 -0300

    se crea pagina de nuevo itinerario

---
+++ src/pages/MyTynerariesCreate.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MyTynerariesSelect from '../components/MyTynerariesSelect'
+
+
+const MyTynerariesCreate = () => {
+  return (
+    
+    <MyTynerariesSelect/>
+    
+
+  )
+}
+
+export default MyTynerariesCreate
