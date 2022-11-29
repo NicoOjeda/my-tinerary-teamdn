ca6d30bdac0f6f018279216c185604f9c08266a1
David Chaban <davidchaban01@gmail.com>
2022-11-29 12:50:45 -0300

    se agrega protected route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Home from "./pages/Home";
-import {Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import WebSiteLayout from './layouts/WebSiteLayout'
 import NotFound from "./pages/NotFound";
 import SignUp from "./pages/SignUp"
@@ -20,59 +20,64 @@ import EditCityForm from "./pages/EditCity";
 import Myitinerariesget from "./pages/Myitinerariesget";
 import EditItineraryForm from "./pages/EditItinerary";
 import { useDispatch, useSelector } from "react-redux";
-import userActions from "./redux/actions/SignInAction"; 
+import userActions from "./redux/actions/SignInAction";
 import Profile from "./pages/Profile";
-import MyTynerariesCreate from './pages/MyTynerariesCreate'
+import ProtectedRoute from "./components/ProtectedRoutes";
 
 
 function App() {
-
-let dispatch = useDispatch()
-let {relogin} = userActions
-
-useEffect(()=>{
-  let token = JSON.parse(localStorage.getItem('token'))
-  console.log(token?.token.user);
-  console.log(token);
-  if(token){
-    dispatch(relogin(token.token.user))
-  }
-})
+  let dispatch = useDispatch()
+  let { relogin } = userActions
+  const user = useSelector(store => store.users)
+  // const [logueado, setLogueado] = (null)
+  // const userLogin = user.logged
 
 
-const tokenList = useSelector(store => store.tokenReducer.tokenList)
-console.log(tokenList);
+console.log(user)
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem('token'))
+    // console.log(token?.token.user);
+    if (token) {
+      dispatch(relogin(token.token.user))
+    }
+  }, [])
 
 
- 
 
+  
 
   return (
     <WebSiteLayout>
-      <Routes> 
-      <Route path='/detailshotels/:id' element={<DetailsHotel/>} ></Route>
-      <Route path='/' element={<Home/>} ></Route>
-      <Route path='*' element={<NotFound/>} ></Route>
-      <Route path='/newhotel' element={tokenList.role === "admin" && tokenList? <NewHotel/> : <Home/>}></Route>
-      <Route path='/myhotels' element={tokenList.role === "admin" && tokenList? <MyHotels/> : <Home/>} ></Route>
-      <Route path='/NewCity' element={tokenList.role === "admin" && tokenList? <NewCity/> : <Home/>} ></Route>
-      <Route path="/mycities" element={tokenList.role === "admin" && tokenList? <MyCitiesget/> : <Home/>}></Route>
-      <Route path='/hotels' element={<Hotels/>}></Route>
-      <Route path='/Cities' element={<Cities/>} ></Route>
-      <Route path='/SignUp' element={<SignUp/>} ></Route>
-      <Route path="/myitineraries" element={tokenList.role === "user" && tokenList !==[]? <Myitinerariesget/> : <Home/>}></Route>
-      <Route path='/myshows' element={tokenList.role === "user" && tokenList !== []? <MyShows/> : <Home/>} ></Route>
-      <Route path='/signin' element={<Signin/>} ></Route>
-      <Route path='/Details/:id' element={<Details/>} ></Route>
-      <Route path='/editcity/:id' element={<EditCityForm/>} ></Route>
-      <Route path='/edithotel/:id/' element={<EditHotel/>} ></Route>
-      <Route path='/editshow/:id/' element={<EditShow/>} ></Route>
-      <Route path="/edititinerary/:id" element={<EditItineraryForm/>}></Route>
-      <Route path="/profile/:id" element={<Profile/>}> </Route>
-      <Route path="/mytynerariescreate" element={<MyTynerariesCreate/>}></Route>
-      <Route path="/profile" element={<Profile/>}> </Route>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='*' element={<NotFound />} ></Route>
+        <Route element={<ProtectedRoute isAllowed={user.logged == false} reDirect={"/"} />}>
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path='/newhotel' element={<NewHotel />} />
+          <Route path="/myitineraries" element={<Myitinerariesget />} />
+          <Route path='/NewCity' element={<NewCity />} />
+          <Route path='/myhotels' element={<MyHotels />} />
+          <Route path="/mycities" element={<MyCitiesget />} />
+          <Route path='/myshows' element={<MyShows />} />
+          <Route path='/detailshotels/:id' element={<DetailsHotel />} />
+          <Route path="/edititinerary/:id" element={<EditItineraryForm />} />
+          <Route path='/edithotel/:id/' element={<EditHotel />} />
+          <Route path='/editcity/:id' element={<EditCityForm />} />
+          <Route path='/Details/:id' element={<Details />} />
+          <Route path='/editshow/:id/' element={<EditShow />} />
+        </Route>
+        <Route element={<ProtectedRoute isAllowed={!!user && user.logged == true} reDirect={"/"} />}>
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/SignUp' element={<SignUp />} />
+        </Route>
+        <Route path='/hotels' element={<Hotels />}></Route>
+        <Route path='/Cities' element={<Cities />} ></Route>
+        {/* <Route path="/mycities/:id" element={<MyCitiesget />}></Route> */}
+        {/* <Route path="/myitineraries/:id" element={<Myitinerariesget />}></Route> */}
       </Routes>
     </WebSiteLayout>
   );
+
+
 }
 export default App;
