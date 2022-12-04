8f5fb5b290d9e0d5768c1002435318c1d89b54f0
David Chaban <davidchaban01@gmail.com>
2022-12-04 14:07:23 -0300

    Se agregan estilos y dependencies de My Reactions

--- src/components/CityDetail.jsx
+++ src/components/CityDetail.jsx
@@ -6,6 +6,9 @@ import "../styles/CityDetails.css";
 import axios from "axios";
 import "../styles/hotelscards.css";
 import "../styles/inputHotels.css";
+import Reaction from "./Reactions";
+
+
 const CityDetail = () => {
   let { id } = useParams();
   // console.log(id);
@@ -26,8 +29,8 @@ const CityDetail = () => {
 
     axios
       .get(`http://localhost:8000/api/itineraries?citiId=${id}`)
-      .then((res) => setDataItinerary(res.data.response))
-      .catch((error) => console.log(error));
+      .then(res => setDataItinerary(res.data.response))
+      .catch(error => console.log(error));
     // console.log(data)
   }, [id]);
 
@@ -61,27 +64,31 @@ const CityDetail = () => {
 
       <div className=".detail-container-Activity">
         <div className="d">
-          {dataItinerary.map((e) => {
-            console.log(e.userId);
+          {dataItinerary.map((data) => {
+            console.log(data.name);
+            console.log(dataItinerary)
             return (
               <>
                 <div className="d-activity-card cityImageDetails  ">
                   <div className="container-activity-detailc">
-                    <div className="activity-title ">{e.name}</div>
+                    <div className="activity-title ">{data.name}</div>
                     <img
                       className="activity-img detail-img1"
-                      src={e.photo}
-                      alt={e.name}
+                      src={data.photo}
+                      alt={data.name}
                     ></img>
 
-                    <div className="activity-info">{e.description}</div>
-                    <div className="activity-info">${e.price}</div>
-                    <div> {e.userId.name}</div>
+                    <div className="activity-info">{data.description}</div>
+                    <div className="activity-info">${data.price}</div>
+                    <div> {data.userId.name}</div>
                     <img
+                    width="100px"
                       className="activity-photo"
-                      src={e.userId.photo}
-                      alt={e.userId.name}
+                      src={data.userId.photo}
+                      alt={data.userId.name}
                     ></img>
+
+                    {/* {console.log(user)} */}
                     {!mostrarOcultar ? (
                       <>
                         <button className="d-btn2" onClick={hide}>
@@ -106,6 +113,8 @@ const CityDetail = () => {
           })}
         </div>
       </div>
+
+<Reaction type="itinerary" eventid={id} />
     </div>
   );
 };
