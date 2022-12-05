5308d1118f4565d52873cd12a065dafe7eeb81ec
David Chaban <davidchaban01@gmail.com>
2022-12-04 23:37:13 -0300

    Se agrega action , reducer , styles y componentes de Reactions

---
+++ src/components/Reaction.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Reaction.css'
+
+
+
+const Reaction = (props) => {
+  let { reaction } = props
+    // console.log(reaction)
+  // let likes =  reaction.userId.finIn
+  // console.log(reaction.lenght)
+  return (
+    <div className='icon-01'> 
+    
+      <img className='img-reaction' src={reaction.icon} onClick=""></img>
+      <p>{reaction.userId.lenght}</p>
+    </div>
+    
+  )
+}
+
+export default Reaction
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -7,6 +7,8 @@ import myItinerariesReducer from './itinerariesReducer';
 import SignInReducer from './SingInReducer'
 import profileReducer from './profileReducer';
 import tokenReducer from './tokenReducer';
+import activityReactionReducer from './activityReactionReducer'
+
 
 const rootReducer = {
         card : cardReducer,
@@ -17,7 +19,8 @@ const rootReducer = {
         myItinerariesReducer,
         users: SignInReducer,
         profileReducer,
-        tokenReducer
+        tokenReducer,
+        activityReactionReducer
 } 
 
 
