8ff89234942c4b8c38f6fd1f9b1348b23c577937
Nico ojeda <nico_ojeda86@hotmail.com>
2023-03-11 20:13:32 -0300

    se ordena codeigo

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -4,24 +4,16 @@ import {Link as LinkRouter} from 'react-router-dom'
 
 export default function CallToAction() {
 
-
-
   return (
     
-<>
- <LinkRouter to='/Cities' className="cities-button" >
-  <button className="btn-1">Cities</button>
-  </LinkRouter>  
-
-
-  <LinkRouter to='/hotels' className="cities-button" >
-  <button className="btn-1">Hotels</button>
-
+  <>
+    <LinkRouter to='/Cities'>
+      <button className="home-btn">Cities</button>
+    </LinkRouter>  
+    <LinkRouter to='/hotels'>
+      <button className="home-btn">Hotels</button>
     </LinkRouter> 
-
-</>
-
-
+  </>
   )
 }
 
